6ed3975c89634d1bc01d480bf943867e0288b686
Emoke Hajdu <hajdu_emoke@outlook.com>
2019-11-17 22:40:05 +0100

    Add new TODOs for Monday

--- src/com/emoke/TODO.java
+++ src/com/emoke/TODO.java
@@ -3,10 +3,14 @@ package com.emoke;
 public class TODO {
     //Sunday todos
     //TODO create options for admin and customer //done ans it works:))
-    //TODO admin options: show list from customers, show list of books, show weather data in dornbirn, order food
-    //TODO customer options->show recommended books, rent a book, retour a book //use date format to compare
-    //TODO move Customer functions to Managment package
-    //TODO add new words to the table wordsNew
-    //TODO create rent a book function
+    //TODO admin options: show list from customers, show list of books, //done
+    //TODO customer options->show recommended books, rent a book, retour a book //done
+    //TODO move Customer functions to Managment package //done
+    //TODO add new words to the table wordsNew //done
+    //TODO create rent a book function //done
 
+    //Monday todos
+    //TODO add new text to BATGA,
+    //TODO add new function to add a (adminManagment)
+    //TODO show weather data in dornbirn, order food
 }
